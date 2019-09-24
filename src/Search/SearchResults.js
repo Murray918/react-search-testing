bbac4198ecb8040ce0ec0a4e9e6a00740830ff27
Andrew Murray <darbyguitars@gmail.com>
2019-09-23 20:11:39 -0500

    pushing branch and changes

--- src/Search/SearchResults.js
+++ src/Search/SearchResults.js
@@ -3,15 +3,14 @@ import PropTypes from 'prop-types'
 
 const SearchResults = ({ articles = [] }) => {
 	//create the li elements
-	const composedArticles = articles.map(({webUrl, webTitle}) => {
- (
+	const composedArticles = articles.map(({webUrl, webTitle}) => (
 			<li key={webTitle}>
 				<a href={webUrl} target="_blank" rel="noopener noreferrer">
 					{webTitle}
 				</a>
 			</li>
 		)
-	})
+			)
 	return (
 		<ul className="search-results">
 			{composedArticles}
